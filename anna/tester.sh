3ed17f7449f29baa9a3a20f30b7bba936acb23e4
Craig Brozefsky <cbrozefs@cisco.com>
2014-11-11 17:47:18 -0500

    Re-arrange the furniture for the future

--- anna/tester.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm ./logs/chat/Anna/chat.log
-rm ./logs/chat/Anna/chat.xml
-# ./server.sh < testcase.txt
--- irctionary.lisp
+++
@@ -1,6 +0,0 @@
-(in-package :cl-user)
-
-(defvar *word-hash* (make-hash-table :test #'equal))
-
-(defun load-hash ()
-  )
